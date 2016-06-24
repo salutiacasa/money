506fd3504828dee679f7dfb9d3568e51452604e0
salutiacasa <m.ciani@estiatechnology.com>
2016-06-24 14:45:49 +0200

    Initial Commit

---
+++ Money/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Money
+//
+//  Created by Marco Ciani on 24/06/16.
+//  Copyright © 2016 money team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
